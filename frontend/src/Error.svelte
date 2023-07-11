aa3a2641ef92059e5ed52021491b5008b17e734e
Bruno <gimcoo@gmail.com>
2023-07-11 08:14:41 +0200

    Se agregan pantallas de error y éxito del proceso

---
+++ frontend/src/Error.svelte
@@ -0,0 +1,14 @@
+<script>
+import Layout from './lib/Layout.svelte'
+</script>
+
+<Layout>
+	<span slot="title">
+		¡Vaya! Nos llevará mas de lo esperado
+	</span>
+	<span slot="subtitle">
+		Para garantizar una configuración óptima, es necesario realizar algunos ajustes de forma manual. Nuestro objetivo principal es brindarle la mejor experiencia posible y le informaremos en breve cuando todo esté en orden para que pueda aprovechar al máximo su entorno.
+	</span>
+	
+	<img src="error.avif">
+</Layout>
---
+++ frontend/src/Ready.svelte
@@ -0,0 +1,20 @@
+<script>
+import Layout from './lib/Layout.svelte'
+
+export let url
+</script>
+
+<Layout>
+	<span slot="title">
+		¡Su entorno HPC está listo para usar!
+	</span>
+	<span slot="subtitle">
+		 Nos complace informarte que el entorno HPC está completamente configurado y listo para que lo utilice en sus investigaciones y proyectos científicos.
+	</span>
+
+	<div class="my-4 flex items-center justify-center">
+    <a href="{url}" target="_blank" class="ui-button ui-button-base ui-button-primary" type="submit">Acceder a Jupyter Notebook</a>
+  </div>
+
+	<img src="ready.avif" alt="Científicos pensando">
+</Layout>
