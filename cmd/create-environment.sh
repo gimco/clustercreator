cb0ce78cea37d11e095dc16c34954c49cb5c5aba
Bruno <gimcoo@gmail.com>
2023-07-11 08:24:42 +0200

    Se modifica para pasar el parámetro a terraform

--- cmd/create-environment.sh
+++ cmd/create-environment.sh
@@ -6,14 +6,13 @@ BASE_TERRAFORM=/opt/clustercreator/terraform
 ENV_NAME=$1
 N_NODOS=$2
 
-# Faltaría recibir ademas los parametros de tipo y nodos
-# Para pasarlos a comando de terraform -var="nnodos=$2"
-
 mkdir $CONFIG/$ENV_NAME
+
 terraform -chdir=$BASE_TERRAFORM \
   apply \
   -auto-approve \
   -no-color \
+  -var="nodos_count=$N_NODOS" \
   -state=$CONFIG/$ENV_NAME/tf-state.json 2>&1 | ts -s >> $CONFIG/$ENV_NAME/creation.log 
 
 # ¿Como comprobar o esperar a que el slurm ya esté listo?
